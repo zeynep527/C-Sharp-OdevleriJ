fa5c9ab31b54cecb5acb4d2676958772de25e164
Zeynep Y <zeynoyldz96@gmail.com>
2021-09-20 19:43:19 +0300

    hata yönetimi

---
+++ Try-Catch-Finally ve Mantıksal Hatalar/Try-Catch-Finally ve Mantıksal Hatalar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Try_Catch_Finally_ve_Mantıksal_Hatalar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
