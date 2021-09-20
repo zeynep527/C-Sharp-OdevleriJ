66d8adae741af124c797d36914caefdc9a3e6391
Zeynep Y <zeynoyldz96@gmail.com>
2021-09-20 16:58:38 +0300

    değişkenler

---
+++ tıp donusumlerı/tıp donusumlerı/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tıp_donusumlerı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
